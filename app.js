0a876d614694a64ef0a83caa57087cb7300f1c6b
Khoa Pham <kpham@Khoas-MBP-2.lan>
2022-01-01 20:36:49 -0500

    add boilerplate and some CSS

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose');
 const Campground = require('./models/campground')
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+app.engine('ejs', ejsMate);
 
 // Connect to Mongo and setup
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -33,11 +39,42 @@ app.get('/campgrounds', async(req,res)=>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds})
 })
+// new campground form
+app.get('/campgrounds/new', (req, res)=>{
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async(req, res)=>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 // show
 app.get('/campgrounds/:id', async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground})
 })
+
+// edit
+app.get('/campgrounds/:id/edit', async (req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground})
+})
+app.put('/campgrounds/:id', async(req,res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+// delete
+app.delete('/campgrounds/:id', async(req, res)=>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+
+})
+
 
 app.listen(3000, ()=>{
     console.log('Serving on port 3000')
--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Logger = require('nodemon/lib/utils/log');
 const Campground = require('../models/campground')
 const cities = require('./cities')
 const {places, descriptors} = require('./seedHelpers');
@@ -22,9 +23,13 @@ const seedDB = async()=>{
     await Campground.deleteMany({});
     for(let i = 0; i<50; i++){
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20) +10;
         const  camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            price: price,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         })
         await camp.save();
     }
